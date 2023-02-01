65b74ba5f084bb31794264515381d9004eeaa9ba
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-02-01 11:12:40 -0800

    create test with good fail

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,17 +1,17 @@
-import Battle from '../src/js/battle.js';
+import Character from './../src/js/characters.js';
 
 describe('Character', () => {
-  let character;
-
-  beforeEach(() => {
-    rectangle = new Rectangle(3,5);
+  
+    test('should correctly create a character object with type, attribute, and classType keys', () => {
+      const character = new Character("fighter", "strength", "jack");
+      expect(character.type).toEqual("fighter");
+      expect(character.attribute).toEqual("strength");
+      expect(character.classType).toEqual("jack");
   });
-
-  test('should correctly test to see character type', () => {
-    expect(character.type).toEqual("glaive");
-    expect(character.attribute).toEqual("nano");
-    expect(character.classType).toEqual("jack");
-  });
-
-
 });
+
+  // test('should correctly test to see character type', () => {
+  //   expect(character.type).toEqual("glaive");
+  //   expect(character.attribute).toEqual("nano");
+  //   expect(character.classType).toEqual("jack");
+  // });
---
+++ src/js/characters.js
@@ -0,0 +1,12 @@
+export default class Character {
+  constructor(type, attribute, classType) {
+    
+  }
+
+//   getBuildType() {
+//     return this.type + this.attriubute + this.classType;
+//   }
+// }
+}
+
+__tests__/Character.test.js src/js/characters.js
